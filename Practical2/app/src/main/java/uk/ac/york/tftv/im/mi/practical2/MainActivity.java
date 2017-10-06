a56581faeba439c60b6f5afa0ae488b4cfa6219e
Ben Kirman <ben@kirman.org>
2017-10-06 17:39:28 +0100

    Updated for 2017

--- Practical2/app/src/main/java/uk/ac/york/tftv/im/mi/practical2/MainActivity.java
+++ Practical2/app/src/main/java/uk/ac/york/tftv/im/mi/practical2/MainActivity.java
@@ -1,64 +1,23 @@
 package uk.ac.york.tftv.im.mi.practical2;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
-import android.widget.TextView;
 
-import java.util.ArrayList;
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
 
-    ArrayList<String> notes;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        if(notes==null) { //if the application has just started, reset the list of notes.
-            notes = new ArrayList<String>();
-            ListView lv = (ListView) findViewById(R.id.notesList); //set the ListView to look at our ArrayList of notes.
-            lv.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, notes));
-        }
-    }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
     }
 
     public void buttonPressed(View view) {
-        Intent i = new Intent(this,CreateNote.class);
-        startActivityForResult(i, 1);
+        Intent i = new Intent(this, ShowLight.class);
+        startActivity(i);
     }
 
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if(resultCode==RESULT_OK) {
-            String new_note = data.getStringExtra("note");
-            notes.add(new_note); //add note to the list of notes
-            ((ArrayAdapter<String>)((ListView) findViewById(R.id.notesList)).getAdapter()).notifyDataSetChanged(); //tell the ListView to update contents.
-        }
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
---
+++ Practical2/app/src/main/java/uk/ac/york/tftv/im/mi/practical2/ShowLight.java
@@ -0,0 +1,64 @@
+package uk.ac.york.tftv.im.mi.practical2;
+
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class ShowLight extends AppCompatActivity implements SensorEventListener {
+
+    private Sensor light;
+    private SensorManager sensor_mgr;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_show_light);
+
+        sensor_mgr = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        light = sensor_mgr.getDefaultSensor(Sensor.TYPE_LIGHT);
+        sensor_mgr.registerListener(this, light, SensorManager.SENSOR_DELAY_NORMAL);
+
+    }
+
+    public void buttonPressed(View view) {
+        Intent i = new Intent(this, MainActivity.class);
+        startActivity(i);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent sensorEvent) {
+        TextView tv = (TextView) findViewById(R.id.lightValue);
+        tv.setText(String.valueOf(sensorEvent.values[0]));
+        //Challenge 3
+        int darkness = 255 - (int)sensorEvent.values[0];
+        if(darkness<0) darkness=0;
+        tv.setTextColor(Color.argb(255,darkness,darkness,darkness));
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
+
+    }
+
+    @Override
+    protected void onPause() {//Challenge 2
+        //we no longer need the sensor data since we are not in focus.
+        super.onPause();
+        sensor_mgr.unregisterListener(this);
+    }
+    @Override
+    protected void onResume() {//Challenge 2
+        //the user has come back! start listening again.
+        super.onResume();
+        sensor_mgr.registerListener(this, light, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+}
