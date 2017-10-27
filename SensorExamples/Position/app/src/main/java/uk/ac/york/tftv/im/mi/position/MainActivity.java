65f887cfee38edb446eb1e2e0dffd348b74f27b5
Ben Kirman <ben@kirman.org>
2017-10-27 15:55:37 +0100

    Updating sensor examples for 2017

--- SensorExamples/Position/app/src/main/java/uk/ac/york/tftv/im/mi/position/MainActivity.java
+++ SensorExamples/Position/app/src/main/java/uk/ac/york/tftv/im/mi/position/MainActivity.java
@@ -1,5 +1,6 @@
 package uk.ac.york.tftv.im.mi.position;
 
+import android.Manifest;
 import android.app.Activity;
 import android.content.Context;
 import android.location.Location;
@@ -48,7 +49,7 @@ public class MainActivity extends Activity {
         }
         catch (SecurityException e) {
             //user rejected request for permission
-            ((TextView) findViewById(R.id.updateTextView)).setText("Error - GPS not enabled");
+            ((TextView) findViewById(R.id.updateTextView)).setText(e.getLocalizedMessage());
         }
     }
 
