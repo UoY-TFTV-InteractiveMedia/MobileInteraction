094c7e9525b8d2731ba561f8a71e32935dfdcb13
Ben Kirman <ben@kirman.org>
2019-10-14 15:53:33 +0100

    fixins

--- Practical2/app/src/main/java/uk/ac/york/tftv/im/mi/practical2/ShowLight.java
+++ Practical2/app/src/main/java/uk/ac/york/tftv/im/mi/practical2/ShowLight.java
@@ -13,7 +13,6 @@ import android.view.View;
 import android.widget.TextView;
 
 public class ShowLight extends AppCompatActivity implements SensorEventListener {
-
     private Sensor light;
     private SensorManager sensor_mgr;
 
