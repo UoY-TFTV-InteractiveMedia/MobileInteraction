5fa030e1806743588f84d1864f9654f8f86dd86d
Ben Kirman <ben@kirman.org>
2020-11-06 13:26:56 +0000

    Update MainActivity.java
    Changed permissions method. might break project but activity code is solid.

--- SensorExamples/Position/app/src/main/java/uk/ac/york/tftv/im/mi/position/MainActivity.java
+++ SensorExamples/Position/app/src/main/java/uk/ac/york/tftv/im/mi/position/MainActivity.java
@@ -1,8 +1,13 @@
 package uk.ac.york.tftv.im.mi.position;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
 import android.Manifest;
-import android.app.Activity;
 import android.content.Context;
+
+import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
@@ -10,7 +15,7 @@ import android.os.Bundle;
 import android.widget.TextView;
 
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
     private LocationManager l_mgr;
 
     @Override
@@ -18,17 +23,30 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-                l_mgr = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+        l_mgr = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+        //do we have permission to get location data?
+        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED &&
+                ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            startSensor();
+        } else {
+            ((TextView) findViewById(R.id.dataView)).setText("Asking for permission");
+            ActivityCompat.requestPermissions(this, new String[]{
+                    Manifest.permission.ACCESS_FINE_LOCATION,
+                    Manifest.permission.ACCESS_COARSE_LOCATION}, 1);
+        }
+    }
 
+    void startSensor(){
         try {
-            l_mgr.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, new LocationListener() {
+
+            l_mgr.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, new LocationListener() {
                 @Override
                 public void onLocationChanged(Location location) {
-                    ((TextView) findViewById(R.id.updateTextView)).setText(
-                            "POSITION:\n"+
-                            "LAT: "+ String.valueOf(location.getLatitude())+"\n"+
-                            "LNG: "+ String.valueOf(location.getLongitude())+"\n"+
-                            "ACC: "+ String.valueOf(location.getAccuracy())+"m\n"
+                    ((TextView) findViewById(R.id.dataView)).setText(
+                            "POSITION:\n" +
+                                    "LAT: " + String.valueOf(location.getLatitude()) + "\n" +
+                                    "LNG: " + String.valueOf(location.getLongitude()) + "\n" +
+                                    "ACC: " + String.valueOf(location.getAccuracy()) + "m\n"
                     );
                 }
 
@@ -47,10 +65,19 @@ public class MainActivity extends Activity {
 
                 }
             });
-        }
-        catch (SecurityException e) {
+        } catch (SecurityException e) {
             //user rejected request for permission
-            ((TextView) findViewById(R.id.updateTextView)).setText(e.getLocalizedMessage());
+            ((TextView) findViewById(R.id.dataView)).setText(e.getLocalizedMessage());
+        }
+    }
+
+    void onRequestPermissionsResult(int requestCode, String[] permissions, int grantResults) {
+        if(grantResults== PackageManager.PERMISSION_GRANTED) {
+            //hooray - we have permission, let's start getting data
+            startSensor();
+        }
+        else {
+            ((TextView) findViewById(R.id.dataView)).setText("Sorry, location permissions required");
         }
     }
 
