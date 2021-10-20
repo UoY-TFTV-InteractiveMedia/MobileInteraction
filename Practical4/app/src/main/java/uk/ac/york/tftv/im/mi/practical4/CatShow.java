68e2c6c5c86b77271dec4027d574fb181e829961
ben <ben@kirman.org>
2021-10-20 18:49:23 +0100

    Fixed usage of OkHTTP for 2021

--- Practical4/app/src/main/java/uk/ac/york/tftv/im/mi/practical4/CatShow.java
+++ Practical4/app/src/main/java/uk/ac/york/tftv/im/mi/practical4/CatShow.java
@@ -1,20 +1,20 @@
 package uk.ac.york.tftv.im.mi.practical4;
 
-import android.os.AsyncTask;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
+import org.jetbrains.annotations.NotNull;
 import org.json.JSONException;
 import org.json.JSONObject;
-
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class CatShow extends AppCompatActivity {
 
@@ -26,89 +26,69 @@ public class CatShow extends AppCompatActivity {
 
     /**
      * This is the method attached to the button onClick property
-     * @param v
      */
-    public void getCat(View v) {
-        new GetCatFact().execute("https://catfact.ninja/fact"); //starts thread to get cat fact
+    public void getFact(View v) {
+        String url = "https://catfact.ninja/fact";
+
+        try {
+            getDataFromServer(url); //Here we call the function that will connect to the server
+        }
+        catch (IOException e) { //The getDataFromServer function can "throw" an IOException if there is an error. We have to "catch" that here, otherwise our entire app will crash.
+            setFact("Error connecting to server"); //put the error on screen
+            return;
+        }
     }
 
     /**
-     * This is the method called by the network thread when it has finished.
-     * @param cat_fact This variable contains the String of the fact
+     * Method that uses OkHTTP to connect to the server
+     * @param url the URL of the remote data source
+     * @throws IOException
+     */
+    void getDataFromServer(String url) throws IOException {
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder().url(url).build();
+
+        client.newCall(request).enqueue(new Callback() { //This is an inner class that will be used to handle the response.
+
+            @Override
+            public void onResponse(@NotNull Call call, @NotNull Response response) throws IOException { //If the response is good...
+                readJSONFact(response.body().string()); //send the JSON we got from the server to the readJSONFact function.
+            }
+
+            @Override
+            public void onFailure(@NotNull Call call, @NotNull IOException e) { //If there is an error in the response...
+                e.printStackTrace(); //Print the error to the console
+            }
+        });
+    }
+
+    /**
+     * Method that takes a string containing JSON, and extracts the Cat Fact.
+     * @param rawJSON A string containing JSON encoded daa
+     */
+    void readJSONFact(String rawJSON) {
+        try {
+            JSONObject json = new JSONObject(rawJSON); //Convert the string into a JSONObject
+            String fact = json.getString("fact"); //Extract the fact from the property "fact", and put it in a string
+            setFact(fact); //call the method that will set the text on screen to show the fact
+        }
+        catch (JSONException e) { //Handle any issues where the JSON is badly formed or invalid
+            setFact("Invalid JSON text");
+        }
+    }
+
+    /**
+     * This is the method called when it has finished.
+     * @param cat_fact This variable contains the String of the fact, to be shown in the textview
      */
     public void setFact(String cat_fact) {
-        TextView tv = (TextView) findViewById(R.id.catFact);
-        tv.setText(cat_fact);
-    }
-
-    /**
-     * This is a custom class that is acting as the thread that will handle our networking
-     * Note it is private, so only CatShow can create it. Also note that it extends a basic threading class that gives it the important behaviour.
-     */
-    private class GetCatFact extends AsyncTask<String, Void, String> {
-        /**
-         * When we call "execute" in getCat(), this is the method that is called. Note that "String..." means it can accept a list of strings.
-         * We need to do it this way, because this is how AsyncTask is wired.
-         */
-        @Override
-        protected String doInBackground(String... urls) {
-            URL url;
-            try {
-                url = new URL(urls[0]); //set the url to be the first parameter sent in the list of urls. See note above.
-
-            } catch (MalformedURLException e) {
-                //Java requires us to handle the error "MalformedURLException", in case we pass an invalid URL.
-                //here we would show some kind of error, but let's just exit and return a blank string:
-                return "";
+        runOnUiThread(new Runnable() { //This section has to happen on the same thread as the user interface.
+            @Override
+            public void run() {
+                TextView tv = (TextView) findViewById(R.id.catFact);
+                tv.setText(cat_fact);
             }
+        });
 
-            StringBuilder sb = new StringBuilder();  //Stringbuilder is a helper class to build strings from remote sources (remember, we don't get all the data at once).
-
-            try {
-                HttpURLConnection connection = (HttpURLConnection) url.openConnection(); //attempt to connect to the server
-                connection.connect();
-                BufferedReader bf = new BufferedReader(new InputStreamReader(connection.getInputStream())); //a bufferedreader is used to read from the data stream. this line hooks it to the connection
-                String line;//this will hold each line of data from the server.
-                while ((line = bf.readLine()) != null)//while the connection still has data arriving
-                {
-                    sb.append(line + "\n"); //add the data to our growing stringbuilder
-                }
-                bf.close();//it has finished, so close the buffer...
-                connection.getInputStream().close(); //and close the connection
-
-                return(sb.toString());//return the entire contents of the stringbuilder as a string. (this will be a JSON formatted string containing the cat fact)
-
-            } catch (IOException e) {
-                //the above functions might raise an exception if there is an error communicating with the remote server (or there is a network issue)
-                //as before, let's just exit and return a blank string again:
-                return "";
-            }
-        }
-
-        /**
-         * onPostExecute displays the results of the AsyncTask. It is here we pass back the data to the main thread of the application.
-         * the String "result" contains the cat fact we retrieved from the server in some JSON
-         */
-        @Override
-        protected void onPostExecute(String result) {
-            if(result.length()==0) {//remember we returned an empty string if there was an error. In that case lets return an error message.
-                setFact("Error!");
-                return;
-            }
-
-            //If there hasn't been an error, "result" now contains a string in JSON format. We only want the fact out of it.
-            //Load the link in a web browser to see the kind of data we get: https://catfact.ninja/fact
-
-            //We have to grab the fact out of the JSON.
-            String fact;
-            try {
-                JSONObject json = new JSONObject(result);
-                fact = json.getString("fact"); //the fact is within the property "fact"
-            }
-            catch(JSONException e) {
-                fact = e.getLocalizedMessage(); //if there is an error in the JSON.
-            }
-            setFact(fact); //once the data has been collected, set the cat fact on the screen
-        }
     }
 }
